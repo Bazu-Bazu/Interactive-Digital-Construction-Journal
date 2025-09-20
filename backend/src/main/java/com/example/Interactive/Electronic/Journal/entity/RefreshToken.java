e4718ebf840ee69a36b3b5fd4943ae586f847173
Bazu-Bazu <iv.dima1304@gmail.com>
2025-09-20 22:36:11 +0300

    Added refresh tokens.

---
+++ backend/src/main/java/com/example/Interactive/Electronic/Journal/entity/RefreshToken.java
@@ -0,0 +1,32 @@
+package com.example.Interactive.Electronic.Journal.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "refresh_tokens")
+@Data
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "expires_at", nullable = false)
+    private Instant expiresAt;
+
+    @Column(nullable = false)
+    private boolean revoked;
+
+}
