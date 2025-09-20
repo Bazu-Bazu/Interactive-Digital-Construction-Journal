d79fbc4f5658fd3bf857cd399851e2f6064b67ec
Bazu-Bazu <iv.dima1304@gmail.com>
2025-09-20 20:06:17 +0300

    Added the possibility of user registration.

---
+++ backend/src/main/java/com/example/Interactive/Electronic/Journal/dto/response/UserResponse.java
@@ -0,0 +1,18 @@
+package com.example.Interactive.Electronic.Journal.dto.response;
+
+import com.example.Interactive.Electronic.Journal.enums.Role;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UserResponse {
+
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String patronymic;
+    private Role role;
+    private String accessToken;
+
+}
--- backend/src/main/java/com/example/Interactive/Electronic/Journal/service/jwt/JwtFilter.java
+++ backend/src/main/java/com/example/Interactive/Electronic/Journal/service/jwt/JwtFilter.java
@@ -32,6 +32,17 @@ public class JwtFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain) throws ServletException, IOException
     {
+        String path = request.getServletPath();
+
+        if (path.contains("/swagger") ||
+            path.contains("/api-docs") ||
+            path.contains("/v3/api-docs") ||
+            path.contains("/auth/"))
+        {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         final String authHeader = request.getHeader("Authorization");
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
