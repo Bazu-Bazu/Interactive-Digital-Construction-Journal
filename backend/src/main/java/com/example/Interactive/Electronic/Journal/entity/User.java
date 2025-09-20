7ebf6697073200e8fdf8ccb54c42c3f7569cac7b
Bazu-Bazu <iv.dima1304@gmail.com>
2025-09-20 22:36:11 +0300

    Added refresh tokens.

--- backend/src/main/java/com/example/Interactive/Electronic/Journal/entity/User.java
+++ backend/src/main/java/com/example/Interactive/Electronic/Journal/entity/User.java
@@ -1,8 +1,12 @@
 package com.example.Interactive.Electronic.Journal.entity;
 
 import com.example.Interactive.Electronic.Journal.enums.Role;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -11,6 +15,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
     private Long id;
 
     @Column(nullable = false, unique = true)
@@ -25,11 +30,22 @@ public class User {
     private String patronymic;
 
     @Column(nullable = false)
+    @JsonIgnore
     private String password;
 
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    @JsonIgnore
     private boolean enabled;
+
+    @OneToMany(
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true,
+            fetch = FetchType.LAZY
+    )
+    @JsonIgnore
+    private List<RefreshToken> refreshTokens = new ArrayList<>();
 
 }
