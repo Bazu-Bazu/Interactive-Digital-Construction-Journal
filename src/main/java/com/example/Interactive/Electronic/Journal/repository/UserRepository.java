133f68056d63869323e677a59062992ccf935c4f
Bazu-Bazu <iv.dima1304@gmail.com>
2025-09-20 11:17:56 +0300

    Added JwtFilter, CustomUSerService and JwtFilter.

--- src/main/java/com/example/Interactive/Electronic/Journal/repository/UserRepository.java
+++ src/main/java/com/example/Interactive/Electronic/Journal/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package com.example.Interactive.Electronic.Journal.repository;
 
-public interface UserRepository {
+import com.example.Interactive.Electronic.Journal.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
 }
--- src/main/java/com/example/Interactive/Electronic/Journal/service/CustomUserDetailsService.java
+++ src/main/java/com/example/Interactive/Electronic/Journal/service/CustomUserDetailsService.java
@@ -1,4 +1,26 @@
-package com.example.Interactive.Electronic.Journal.service.jwt;
+package com.example.Interactive.Electronic.Journal.service;
 
-public class CustomUserDetailsService {
+import com.example.Interactive.Electronic.Journal.dto.CustomUserDetails;
+import com.example.Interactive.Electronic.Journal.entity.User;
+import com.example.Interactive.Electronic.Journal.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found."));
+
+        return new CustomUserDetails(user);
+    }
+
 }
