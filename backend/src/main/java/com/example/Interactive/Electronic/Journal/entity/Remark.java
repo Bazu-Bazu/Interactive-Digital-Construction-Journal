cf2e7d6a16a612b3d0e497eccdc9daec9edd19cd
Bazu-Bazu <iv.dima1304@gmail.com>
2025-09-25 20:21:33 +0300

    Added remark entity.

---
+++ backend/src/main/java/com/example/Interactive/Electronic/Journal/entity/Remark.java
@@ -0,0 +1,36 @@
+package com.example.Interactive.Electronic.Journal.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "remarks")
+@Data
+public class Remark {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "created_at", nullable = false)
+    LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    String description;
+
+    @Column(nullable = false)
+    private List<Double> coordinates;
+
+    @Column(nullable = false)
+    private Boolean fixed;
+
+//    @ElementCollection
+//    @CollectionTable(name = "remark_files", joinColumns = @JoinColumn(name = "remark_id"))
+//    @Column(name = "file_url")
+//    private List<String> urls = new ArrayList<>();
+
+}
